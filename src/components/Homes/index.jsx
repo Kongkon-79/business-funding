36403a53882b47684a6174981002fd4e04be1622
Kongkon-79 <kongkonjowarder@gmail.com>
2025-02-03 14:28:25 +0600

    banner section add

---
+++ src/components/Homes/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './Banner/Banner';
+
+const HomeAllComponents = () => {
+    return (
+        <div>
+            <Banner/>
+        </div>
+    );
+};
+
+export default HomeAllComponents;
