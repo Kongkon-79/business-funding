cc65a7f865eb490bdbf9286ab84a6ca91575873e
Kongkon-79 <kongkonjowarder@gmail.com>
2025-02-04 10:28:46 +0600

    small device navbar design and whyChooseUs section problem solve

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import { Menu } from 'lucide-react';
-import React from 'react';
+import { Menu, X } from 'lucide-react';
+import React, { useState } from 'react';
 import Button from '../../common/Button';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className='px-4 md:px-[50px] lg:px-[100px] xl:px-[150px] 2xl:px-[212px] py-[15px] md:pt-[35px] lg:pt-[47px] md:pb-[45px] lg:pb-[65px] roboto_font'>
+        <div className='px-4 md:px-[50px] lg:px-[100px] xl:px-[150px] 2xl:px-[212px] 3xl:px-[230] 4xl:px-[260px] 5xl:px-[285px] py-[15px] md:pt-[35px] lg:pt-[47px] md:pb-[45px] lg:pb-[65px] roboto_font bg-white sticky top-0 z-50'>
             <div className='flex items-center justify-between'>
                 {/* logo  */}
                 <div>
@@ -22,14 +23,30 @@ const Navbar = () => {
                 </div>
                 {/* button  */}
                 <div >
-                    <Button/>
+                    <Button />
                 </div>
 
                 {/* small device  */}
                 <div className='block md:hidden'>
-                    <Menu />
+                    {
+                        isOpen ? <X onClick={() => setIsOpen(!isOpen)} /> : <Menu onClick={() => setIsOpen(!isOpen)} />
+                    }
+
                 </div>
+
             </div>
+            <div className={`${isOpen ? "w-[70%] left-0" : "w-full -left-[100%]"} h-[40%] fixed top-[77px] z-20 bg-white`}>
+                {
+                    isOpen && <ul className='flex flex-col items-center gap-[32px]'>
+                        <li className='text-base font-normal leading-[28px] text-primary hover:text-secondary hover:underline cursor-default pt-4'>Home</li>
+                        <li className='text-base font-normal leading-[28px] text-primary hover:text-secondary hover:underline cursor-default'>How it Works</li>
+                        <li className='text-base font-normal leading-[28px] text-primary hover:text-secondary hover:underline cursor-default'>Funding Option</li>
+                        <li className='text-base font-normal leading-[28px] text-primary hover:text-secondary hover:underline cursor-default'>Resources</li>
+                        <li className='text-base font-normal leading-[28px] text-primary hover:text-secondary hover:underline cursor-default'>About Us</li>
+                    </ul>
+                }
+            </div>
+            {/* small device end  */}
         </div>
     );
 };
